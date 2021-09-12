ebac0571a1e3314ce0a347720fc5fe5da0819ebb
Firoz Hossain <68817831+dfiiroz@users.noreply.github.com>
2021-09-13 00:34:27 +0600

    Announcement bar remove on scroll

---
+++ sticky.js
@@ -0,0 +1,6 @@
+function scrollFunction() {
+     document.querySelector(".announcement-bar").style.visibility = "hidden";   //To hide the element.
+   document.querySelector(".site-header").style.marginTop= "-8vh";
+}
+
+window.onscroll = scrollFunction;
